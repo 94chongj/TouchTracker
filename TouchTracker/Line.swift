f89a80b4300996f62c54cb69752c8dac33495dc9
Jonathan Chong <jonathan@jonathan.local>
2018-08-30 09:15:28 -0700

    Ch19 silver-gold challenge completed. Attempting platinum Challenge next.

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,16 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Jonathan Chong on 8/29/18.
+//  Copyright © 2018 Jonathan Chong. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+    var lineWidth: CGFloat = 10
+}
